4c7af44a57f1d4001cd946f1ecbadbcccd4992c9
Almas Javed <alphathefinisher@gmail.com>
2025-07-30 11:36:55 -0700

    adding HeroComponent and ServicesPage

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,26 +1,32 @@
+<script>
+  function changeStyleOnMouseOver(elementId, style){
+    let element = document.getElementById(elementId);
+    element.style.setProperty = style
+  }
+</script>
 <section
   class=" text-white py-4 d-flex"
-  style="height: 80vh;background-color: #008080"
+  style="height: 80vh;background-color: #023E8A; align-items:center"
 >
-  <div class="container w-50">
-    <h1 class="display-10 fw-bold">
+  <div class="container w-50" style=" margin-left: 64px;">
+    <h1 class="display-11 fw-bold">
       Your Errands, Handled by Our Trusted Team.
     </h1>
     <h1 class="display-10 fw-bold">Sit Back — We’ll Take Care of It.</h1>
-    <p class=" mt-3">
+    <!-- <p class=" mt-3">
       Need to drop off a document, pick up groceries, or get something done in
       the city while you're miles away? Let our team handle it for you —
       quickly, reliably, and hassle-free.
-    </p>
+    </p> -->
     <div class="mt-4 pe-3">
-      <button class="btn btn-outline-light rounded-pill border-2 font-semibold pe-5">
-        <a
+      <a
         href="/contact"
-        class="text-decoration-none text-white fs-5"
+        id="letsGo"
+        class="btn btn-outline-light rounded-pill border-2 font-semibold pe-5 text-white fs-5 text-decoration-none"
+        onmouseover={changeStyleOnMouseOver(id,"color='#023E8A'; borderColor='#023E8A';")}
       >
         Let's Go
       </a>
-      </button>
     </div>
   </div>
   <div class=" w-50">djfkldsj</div>
