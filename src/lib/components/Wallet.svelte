a06ee0ee5feffe2cf4c407ef614d82f503e20eb7
Almas Javed <alphathefinisher@gmail.com>
2025-09-11 19:53:06 +0500

    added borders to wallet info

--- src/lib/components/Wallet.svelte
+++ src/lib/components/Wallet.svelte
@@ -1,34 +1,36 @@
+<script>
+  const walletDetails = [
+    {
+      heading : "Wallet Details",
+      detail : "Your wallet helps you manage payments on the platform. Add funds, post tasks, and stay in control."
+    },
+    {
+      heading : "Deposit & Spend",
+      detail : "Add money to your wallet and use it to post or accept jobs. If you have Rs500 you can post or take up jobs worth up to Rs 500"
+    },
+    {
+      heading : "Auto Refunds",
+      detail : "If a task isn’t completed as agreed, the amount is refunded back to your wallet."
+    },
+    {
+      heading : "Easy Budgeting",
+      detail : " Your wallet balance gives you a clear view of how much you can spend. No surprise charges."
+    }
+
+  ]
+</script>
+
 <section class="py-5" style=" min-height: 100vh;">
   <div class="container text-center text-white">
-    <h2 class="fw-bold mb-3" style="color: #FFD700;">Wallet Details</h2>
-    <p class="text-light mb-5" style="color: #f1f1f1; max-width: 700px; margin: 0 auto;">
-      Your wallet helps you manage payments on the platform. Add funds, post tasks, and stay in control.
-    </p>
-
-    <div class="mb-4 p-4 rounded shadow" 
+    {#each walletDetails as item}
+      <div class="mb-4 p-4 border border-secondary rounded shadow" 
          style="background-color: #111; border-left: 5px solid #FFD700; text-align: left;">
-      <h3 class="fw-bold mb-2" style="color: #FFD700;">Deposit & Spend</h3>
+      <h3 class="fw-bold mb-2" style="color: #FFD700;">{item.heading}</h3>
       <p class="text-white-50">
-        Add money to your wallet and use it to post or accept jobs. If you have <strong style="color: #FFD700;">Rs. 500</strong>, 
-        you can post or take up jobs worth up to <strong style="color: #FFD700;">Rs. 500</strong>.
+        {item.detail}
       </p>
-    </div>
-
-    <div class="mb-4 p-4 rounded shadow" 
-         style="background-color: #111; border-left: 5px solid #FFD700; text-align: left;">
-      <h3 class="fw-bold mb-2" style="color: #FFD700;">Auto Refunds</h3>
-      <p class="text-white-50">
-        If a task isn’t completed as agreed, the amount is <strong style="color: #FFD700;">refunded</strong> back to your wallet.
-      </p>
-    </div>
-
-    <div class="mb-4 p-4 rounded shadow" 
-         style="background-color: #111; border-left: 5px solid #FFD700; text-align: left;">
-      <h3 class="fw-bold mb-2" style="color: #FFD700;">Easy Budgeting</h3>
-      <p class="text-white-50">
-        Your wallet balance gives you a clear view of how much you can spend. 
-        <strong style="color: #FFD700;">No surprise charges</strong>.
-      </p>
-    </div>
+    </div>  
+    {/each}
+       
   </div>
 </section>
