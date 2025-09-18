f0d6d4895215734227ddb55b6ac365ac96115daf
Almas Javed <alphathefinisher@gmail.com>
2025-09-18 14:06:44 +0500

    added reusable button components

--- src/lib/components/CTA.svelte
+++ src/lib/components/CTA.svelte
@@ -1,12 +1,25 @@
+<script>
+  import AppButton from "./UIComponents/AppButton.svelte";
+</script>
 <section class=" py-5">
   <div class="container text-center">
-   <h3 class="fw-bold mb-3">Ready to Get Started?</h3>
-    <p class="text-secondary">Join thousands already using TaskConnect to make life easier and earn extra income.</p>
+    <h3 class="fw-bold mb-3">Ready to Get Started?</h3>
+    <p class="text-secondary">
+      Join thousands already using TaskConnect to make life easier and earn
+      extra income.
+    </p>
     <div class="d-flex justify-content-center gap-3 mt-4">
-      <a href="/post-task" class="btn btn-warning fw-bold px-4">Start Posting Tasks</a>
-      <a href="/become-helper" class="btn btn-outline-warning fw-bold px-4">Become a Helper</a>
+      <AppButton
+        href="/post-task"
+        classes="btn-warning fw-bold px-4"
+        label="Start Posting Tasks"
+      />
+
+      <AppButton
+        href="/become-helper"
+        classes="btn-outline-warning fw-bold px-4"
+        label="Become a Helper"
+      />
     </div>
   </div>
 </section>
-
-
